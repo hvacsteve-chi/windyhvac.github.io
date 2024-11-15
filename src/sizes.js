a830a4b2eef614f3ad3d6c63f725a53f8ae270da
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2024-11-15 01:35:01 -0600

    Update sizes.js

--- src/sizes.js
+++ src/sizes.js
@@ -1,17 +1,25 @@
 var docEl = document.documentElement;
 var pageWidth = docEl.clientWidth;
 console.log(pageWidth + "px wide");
-var hh = document.querySelector("header").clientHeight;
-var mh = document.querySelector("main").clientHeight;
-var fh = document.querySelector("footer").clientHeight;
+
+var h = document.querySelector("header");
+var m = document.querySelector("main");
+var f = document.querySelector("footer");
+
+var hh = h.clientHeight;
+var mm = m.clientHeight;
+var ff = f.clientHeight;
+
+var hw = h.clientWidth;
+var mw = m.clientWidth;
+var fw = f.clientWidth;
+
 var headerSpan = document.querySelector("#headerHeight");
 var mainSpan = document.querySelector("#mainHeight");
 var footerSpan = document.querySelector("#footerHeight");
 
 export default function sizes() {
-
   headerSpan.innerText = hh + "px";
   mainSpan.innerText = mh + "px";
   footerSpan.innerText = fh + "px";
-
 }
