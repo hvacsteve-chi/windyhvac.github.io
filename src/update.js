d66d1091cb58056d2c00a29c848dfc99f75c0983
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2025-05-08 23:59:52 -0500

    Update update.js

--- src/update.js
+++ src/update.js
@@ -2,7 +2,12 @@ import format from 'date-fns/format';
 import sizes from './sizes.js';
 
 var span = document.querySelector('#time-now');
+var spansize = document.querySelector('#mainHeight');
 
+export function sizes(){
+    spansize.textContent = mh.clientWidth();
+}
+    
 export default function update() {
 	span.textContent = format(new Date(), 'h:mm:ssa');
 	setTimeout(update, 1000);
